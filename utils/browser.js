04d8659530f960eab6698a12fba89d50a36f840b
Markus Hederström <markus.hederstrom@teliacompany.com>
2023-06-08 14:08:52 +0200

    puppeteer

--- utils/browser.js
+++ utils/browser.js
@@ -1,12 +1,19 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
 
 async function startBrowser() {
   let browser;
   try {
     console.log('Opening the browser......');
-    browser = await puppeteer.launch({
+    browser = await chromium.puppeteer.launch({
+      // browser = await puppeteer.launch({
+      // headless: true,
+      // args: ['--disable-setuid-sandbox'],
+      // ignoreHTTPSErrors: true,
+      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
       headless: true,
-      args: ['--disable-setuid-sandbox'],
       ignoreHTTPSErrors: true,
     });
   } catch (err) {
