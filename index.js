eac54a735f6ee095b9bb535949ca0978135e4b87
Markus Hederström <markus.hederstrom@teliacompany.com>
2023-06-08 13:10:32 +0200

    test

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const multer = require('multer');
-const cors = require('cors');
-const fs = require('fs');
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+// const multer = require('multer');
+// const cors = require('cors');
+// const fs = require('fs');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const { resizeImage } = require('./utils/resize-image');
-const { getPlayers } = require('./utils/get-players');
-const { scrape } = require('./utils/scraper');
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
+// const { resizeImage } = require('./utils/resize-image');
+// const { getPlayers } = require('./utils/get-players');
+// const { scrape } = require('./utils/scraper');
 
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/image', upload.array('files'), async (req, res) => {
-  console.log(req.body);
-  console.log(req.files);
+// app.post('/image', upload.array('files'), async (req, res) => {
+//   console.log(req.body);
+//   console.log(req.files);
 
-  if (req.files && req.files[0]) {
-    const file = req.files[0];
-    const resizedImagePath = `${file.path}_small.jpg`;
-    await resizeImage(file.path, resizedImagePath);
+//   if (req.files && req.files[0]) {
+//     const file = req.files[0];
+//     const resizedImagePath = `${file.path}_small.jpg`;
+//     await resizeImage(file.path, resizedImagePath);
 
-    const players = await getPlayers(resizedImagePath);
+//     const players = await getPlayers(resizedImagePath);
 
-    const results = await Promise.all(
-      players.map(async (player) => {
-        const price = await scrape(player.url);
-        return {
-          name: player.name,
-          price: price && price.length ? price[0] : 0,
-          url: player.url,
-        };
-      })
-    );
+//     const results = await Promise.all(
+//       players.map(async (player) => {
+//         const price = await scrape(player.url);
+//         return {
+//           name: player.name,
+//           price: price && price.length ? price[0] : 0,
+//           url: player.url,
+//         };
+//       })
+//     );
 
-    // remove uploaded files
-    fs.unlinkSync(file.path);
-    fs.unlinkSync(resizedImagePath);
+//     // remove uploaded files
+//     fs.unlinkSync(file.path);
+//     fs.unlinkSync(resizedImagePath);
 
-    console.log('players', results);
-    return res.json(results);
-  }
+//     console.log('players', results);
+//     return res.json(results);
+//   }
 
-  return res.json({ done: true });
-});
+//   return res.json({ done: true });
+// });
 
 app.get('/', (req, res) => {
   return res.json({ hello: true });
@@ -70,20 +70,20 @@ app.get('/', (req, res) => {
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
